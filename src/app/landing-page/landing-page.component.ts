81952404fc97e32ba17a0a98a20ed6c9007b7039
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-02-19 16:39:23 +0100

    Added nav bar and font

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
