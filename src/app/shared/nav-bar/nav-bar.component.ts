04cccaa94dc94f7184154ed37805bac2e12eb906
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-02-25 15:31:01 +0100

    Nav links are highlighted when clicked on it

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  highlightedLinkIndex: number = 0;
 
+  highlightLink(index: number) {
+    this.highlightedLinkIndex = index;
+  }
 }
