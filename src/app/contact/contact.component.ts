5ea40339c3d88abd43b6a5d6e49686aa6f646980
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-03-04 15:54:30 +0100

    Fix formatting issues and improve responsiveness

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -30,69 +30,69 @@ export class ContactComponent implements OnInit {
       message: ''
     }
 
-    mailTest = true;
+  mailTest = true;
 
-    post = {
-      endPoint: 'https://deineDomain.de/sendMail.php',
-      body: (payload: any) => JSON.stringify(payload),
-      options: {
-        headers: {
-          'Content-Type': 'text/plain',
-          responseType: 'text',
-        },
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
       },
+    },
+  };
+
+  isVisible: boolean = false;
+
+  ngOnInit(): void {
+    this.createObserver();
+  }
+
+  createObserver() {
+    const callback = (entries: any) => {
+      entries &&
+        entries.forEach((entry: any) => {
+          if (entry.isIntersecting) {
+            this.isVisible = true;
+            target!.style.height = '100px';
+          }
+        });
     };
 
-    isVisible: boolean = false;
-
-    ngOnInit(): void {
-      this.createObserver();
+    const options = {
+      rootMargin: '0px 0px -400px 0px'
     }
 
-    createObserver() {
-      const callback = (entries: any) => {
-        entries &&
-          entries.forEach((entry: any) => {
-            if (entry.isIntersecting) {
-              this.isVisible = true;
-              target!.style.height = '100px';
-            }
-          });
-      };
-  
-      const options = {
-        rootMargin: '0px 0px -400px 0px'
-      }
-  
-      const observer = new IntersectionObserver(callback, options);
-      const target = document.getElementById('contact');
-      target && observer.observe(target);
+    const observer = new IntersectionObserver(callback, options);
+    const target = document.getElementById('contact');
+    target && observer.observe(target);
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
-  
-    onSubmit(ngForm: NgForm) {
-      if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-        this.http.post(this.post.endPoint, this.post.body(this.contactData))
-          .subscribe({
-            next: (response) => {
-  
-              ngForm.resetForm();
-            },
-            error: (error) => {
-              console.error(error);
-            },
-            complete: () => console.info('send post complete'),
-          });
-      } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-        
-        ngForm.resetForm();
-      }
-    }
+  }
 
   toggleCheckBox() {
-      if (!this.checked) {
-        this.checked = true;
-      } else {
-        this.checked = false;
-      }
+    if (!this.checked) {
+      this.checked = true;
+    } else {
+      this.checked = false;
+    }
   }
 }
