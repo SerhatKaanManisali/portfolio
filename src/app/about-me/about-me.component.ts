776e195d11d1102257fa4009aada386772fea27d
SerhatKaanManisali <serhat.manisali.02@gmail.com>
2024-03-06 18:22:15 +0100

    Update about-me.component.ts with corrected spelling of "frontend"

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -23,7 +23,7 @@ export class AboutMeComponent implements OnInit {
     paragraph3: string;
   } = {
       title: "About Me",
-      paragraph1: "I am an enthusiastic front-end developer and have been interested in information technology since my school days. At the Developer Akademie, I was able to pursue my passion and acquire knowledge in front-end development by developing various projects.",
+      paragraph1: "I am an enthusiastic frontend developer and have been interested in information technology since my school days. At the Developer Akademie, I was able to pursue my passion and acquire knowledge in front-end development by developing various projects.",
       paragraph2: "I have gained experience in fundamental web technologies, as well as in Angular, through developing web applications both independently and in a team. I am always eager to learn new technologies and improve my skills.",
       paragraph3: "During development, I always strive for clear communication and clean code. After all, both aspects are essential for smooth teamwork. My problem-solving skills, combined with a focus on user-friendly and intuitive interfaces, round out my projects.",
     }
@@ -35,7 +35,7 @@ export class AboutMeComponent implements OnInit {
     paragraph3: string;
   } = {
       title: "Über mich",
-      paragraph1: "Ich bin ein begeisterter Frontend-Entwickler und interessiere mich schon seit meiner Schulzeit für Informationstechnologie. Bei der Developer Akademie konnte ich meiner Leidenschaft nachgehen und durch die Entwicklung verschiedener Projekte Kenntnisse in der Frontend-Entwicklung aneignen.",
+      paragraph1: "Ich bin ein begeisterter Frontend Entwickler und interessiere mich schon seit meiner Schulzeit für Informationstechnologie. Bei der Developer Akademie konnte ich meiner Leidenschaft nachgehen und durch die Entwicklung verschiedener Projekte Kenntnisse in der Frontend-Entwicklung aneignen.",
       paragraph2: "Ich habe Erfahrung in grundlegenden Webtechnologien sowie in Angular durch die selbstständige und im Team durchgeführte Entwicklung von Webanwendungen gesammelt. Ich bin immer bestrebt, neue Technologien zu erlernen und meine Fähigkeiten zu verbessern.",
       paragraph3: "Bei der Entwicklung lege ich großen Wert auf klare Kommunikation und sauberen Code. Schließlich sind beide Aspekte für eine reibungslose Teamarbeit essenziell. Meine Problemlösungskompetenz, kombiniert mit einem Fokus auf benutzerfreundliche und intuitive Schnittstellen, runden meine Projekte ab.",
     }
